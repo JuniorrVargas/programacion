f1dab5354cde98aafd897aa341d659511270e137
Juniorr Vargas <ixsatanasz@gmail.com>
2024-02-23 09:01:48 -0500

    Subida de actividad 1

---
+++ main.py
@@ -0,0 +1,13 @@
+from persona import Persona
+
+p=Persona("junior","apellido")
+print(p.Obtenernombre())
+
+p=Persona("junior","vargas")
+print(p.Obtenerapellido())
+
+p=Persona("junior","vargas","xxjuniorm4@gmail.com")
+print(p.Obtenercorreo())
+
+p=Persona("junior","vargas","xxjuniorm4@gmail.com","3107442387")
+print(p.Obtenertelefono())
---
+++ persona.py
@@ -0,0 +1,17 @@
+class Persona:
+    def __init__(self,nombre,apellido,correo,telefono):
+        self.nombre=nombre
+        self.apellido=apellido
+        self.correo=correo
+        self.telefono=telefono
+def Obtenernombre (self):
+        return f'mi nombre es {self.nombre}'
+def Obtenerapellido (self):
+        return f'mi apellido es {self.apellido}'
+def Obtenercorreo (self):
+        return f'mi correo es {self.correo}'
+def Obtenertelefono (self):
+        return f'mi correo es {self.telefono}'
+    
+
+print("nombre","junior","apellido","vargas","correo","juniorvargas6661@gmail.com","telefono","3107442387")
